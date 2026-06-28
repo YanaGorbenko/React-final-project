aa028ca18ba517da12c81685927e00e62bd020dd
Yana Horbenko <yanagorbenko33@gmail.com>
2026-06-28 11:40:14 +0300

    change api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
+const getBaseURL = () => {
+  if (import.meta.env.PROD) {
+    return 'https://gamesdb.onrender.com';
+  }
+  return '/api';
+};
+
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: getBaseURL(),
   withCredentials: true,
 });
